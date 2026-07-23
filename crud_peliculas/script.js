46a2fb4a3073a4b9fae76ebd6e764f39d2c0b2b8
yinibaher <jefeibarrahe@ittepic.edu.mx>
2026-07-23 16:39:48 -0700

    Actualizar URL de la API

--- crud_peliculas/script.js
+++ crud_peliculas/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/peliculas";
+const API_URL = "https://api-proyect-nine.vercel.app/";
 
 const formulario = document.getElementById("formulario");
 const id = document.getElementById("id");
